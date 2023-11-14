9978e4bcbbe95378e4a7f40e5fc0e8c6584c4c75
Nhân <hi@imnhan.com>
2023-11-14 20:20:11 +0700

    let BE identify itself

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,14 @@
+import os
+
 from bottle import route, run
+
+BACKEND_ENV = os.environ["BACKEND_ENV"]
 
 
 @route("/api/hello/")
 def hello():
-    return "Hello friend."
+    return f"Greetings from {BACKEND_ENV}."
 
 
+print(f"Starting [{BACKEND_ENV}] server")
 run(host="localhost", port=8000, debug=True)
