9f9daaf041182df64da23b50e09e6c0cf030fab1
Nhân <hi@imnhan.com>
2023-11-15 02:15:52 +0700

    add router; do / and /aad-callback/
    One niggle: apparently react-router-dom removes the hash fragment, so we
    lose the token string sent from AAD.

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,17 +1,72 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import { useEffect } from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const BE_URL = process.env.REACT_APP_BACKEND_URL;
+
+const CALLBACK_PATH = "/aad-callback/";
+const CALLBACK_URL = encodeURIComponent(
+  `http://host.local:3000${CALLBACK_PATH}`,
+);
+const AUTH_URL = `${BE_URL}/.auth/login/aad?post_login_redirect_uri=${CALLBACK_URL}`;
+
+function getToken() {
+  return localStorage.getItem("authToken");
+}
+
+function setToken(token) {
+  localStorage.setItem("authToken", token);
+}
+
+const Home = () => {
+  const token = getToken();
+
+  if (token === null) {
+    return (
+      <div>
+        <h1>Not logged in</h1>
+        <p>
+          Authenticate yourself here: <a href={AUTH_URL}>{AUTH_URL}</a>
+        </p>
+      </div>
+    );
+  }
+
+  return <div>Token: {token}</div>;
+};
+
+const AADCallback = () => {
+  // Zoomers' componentDidMount
+  useEffect(() => {
+    const hash = window.location.hash;
+    if (!hash) {
+      console.log("Empty hash. Aborted.");
+      return;
+    }
+    const blob = decodeURIComponent(hash.slice("#token=".length));
+    const token = JSON.parse(blob).authenticationToken;
+    console.log(">> token:", token);
+    setToken(token);
+  }, []);
+  return <p>Callback</p>;
+};
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: CALLBACK_PATH,
+    element: <AADCallback />,
+  },
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
