e752fd00d67cf97c71b75401144b75504cce1387
Nhân <hi@imnhan.com>
2023-11-14 20:36:49 +0700

    add componentdidmount

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect } from "react";
 
 function App() {
+  // Zoomers' componentDidMount()
+  useEffect(() => {
+    console.log("Heeeey");
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
