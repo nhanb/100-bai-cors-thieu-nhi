aa01617e12e0d99ce41aaa5a2ac99513688a8661
Nhân <hi@imnhan.com>
2023-11-15 15:59:37 +0700

    finally working

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
@@ -20,37 +20,104 @@ function setToken(token) {
   localStorage.setItem("authToken", token);
 }
 
+function logout() {
+  localStorage.clear();
+  window.location.reload();
+}
+
 const Home = () => {
   const token = getToken();
+  const [respText, setRespText] = useState("");
+  const [errText, setErrText] = useState("");
 
   if (token === null) {
     return (
       <div>
         <h1>Not logged in</h1>
+        <button onClick={callApi}>Call API</button> (will fail)
         <p>
-          Authenticate yourself here: <a href={AUTH_URL}>{AUTH_URL}</a>
+          Authenticate yourself here:{" "}
+          <a rel="noreferrer" href={AUTH_URL}>
+            {AUTH_URL}
+          </a>
         </p>
       </div>
     );
   }
 
-  return <div>Token: {token}</div>;
+  // We have a token now. Use it.
+  function callApi(useToken) {
+    setErrText("");
+    setRespText("");
+
+    const tok = useToken ? token : "";
+
+    fetch(`${BE_URL}/api/hello/`, {
+      headers: {
+        "X-ZUMO-AUTH": tok,
+      },
+    })
+      .then((resp) => resp.text())
+      .then(setRespText)
+      .catch((err) => {
+        setErrText(err.toString());
+      });
+  }
+
+  return (
+    <div>
+      <button onClick={logout}>Logout</button>
+      <p>Token: {token}</p>
+      <button
+        onClick={() => {
+          callApi(true);
+        }}
+      >
+        Call API
+      </button>{" "}
+      <button
+        onClick={() => {
+          callApi(false);
+        }}
+      >
+        Call API without token
+      </button>
+      {respText ? (
+        <p>
+          Response from api: <b>{respText}</b>
+        </p>
+      ) : (
+        ""
+      )}
+      {errText ? (
+        <p>
+          ERROR: <b>{errText}</b>
+        </p>
+      ) : (
+        ""
+      )}
+    </div>
+  );
 };
 
 const AADCallback = () => {
+  const [msg, setMsg] = useState("n/a");
+
   // Zoomers' componentDidMount
   useEffect(() => {
     const hash = window.location.hash;
     if (!hash) {
-      console.log("Empty hash. Aborted.");
+      setMsg("Empty hash. Aborted.");
       return;
     }
+    setMsg(`Received token: ${hash}`);
     const blob = decodeURIComponent(hash.slice("#token=".length));
     const token = JSON.parse(blob).authenticationToken;
-    console.log(">> token:", token);
     setToken(token);
+    window.location = "/";
   }, []);
-  return <p>Callback</p>;
+
+  return <p>{msg}</p>;
 };
 
 const router = createBrowserRouter([
